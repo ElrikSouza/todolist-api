ecc34c5495b7b0b6e89bce248d3c9e4ab715a442
Elrik <72892051+ElrikSouza@users.noreply.github.com>
2020-10-15 13:45:44 -0400

    added validation to env variables

---
+++ src/env.ts
@@ -0,0 +1,23 @@
+import Joi from 'joi';
+
+interface Env {
+    PORT: number;
+    ALLOWED_ORIGIN: string;
+    CONNECTION_STRING: string;
+    JWT_SECRET: string;
+}
+
+const envSchema = Joi.object({
+    PORT: Joi.number().positive(),
+    ALLOWED_ORIGIN: Joi.string().hostname().required(),
+    CONNECTION_STRING: Joi.string().required(),
+    JWT_SECRET: Joi.string().required(),
+});
+
+const validationResult = envSchema.validate(process.env);
+
+if (validationResult.error) {
+    process.exit(1);
+}
+
+export const { PORT = 4000, ALLOWED_ORIGIN, CONNECTION_STRING, JWT_SECRET }: Env = validationResult.value;
